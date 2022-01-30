9aafb1a9d80241895a4df67681c9949b7c315ada
SergeRomanenko13 <SerhiiRomanenko13@gmail.com>
2022-01-31 01:08:49 +0200

    1st commit

--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export function Catalog() {
+  const [data, setData] = useState(null);
+  const [status, setStatus] = useState("initial");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://61f5558a62f1e300173c40f3.mockapi.io/products")
+      .then(response => response.json())
+      .then(date => {
+        setData(date);
+      })
+      .catch((err) => {
+        console.error('Error:', err);
+      });
+  },[]);
+  console.log(data)
+
   return <main>
-    This page is under construction ;)
+    {data !== null ?
+      data.map((product) => {
+        return <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.price}</p>
+          <p>{product.description}</p>
+          <p>{product.rating}</p>
+          <p>{product.photo}</p>
+        </div>
+      })
+       : <h2>Empty</h2>
+    }
   </main>
 }
