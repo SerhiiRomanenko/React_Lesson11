8747ccdfcefb6b40af2cf9f04063cc7f76d2a1b2
SergeRomanenko13 <SerhiiRomanenko13@gmail.com>
2022-01-31 10:36:52 +0200

    1st commit

--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,4 +1,13 @@
 import React, {useEffect, useState} from "react";
+import "./Catalog.scss";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export function Catalog() {
   const [data, setData] = useState(null);
@@ -23,24 +32,49 @@ export function Catalog() {
         setError(err);
         setStatus("error");
       });
-  },[]);
+  }, []);
   console.log(data)
 
-  return <main>
+  return <main className="catalog">
     {status === "initial" || status === 'loading' ?
-      <h2>
-        Loading...
-      </h2>
-       :
-      data.map((product) => {
-        return <div key={product.id}>
-          <h2>{product.title}</h2>
-          <p>{product.price}</p>
-          <p>{product.description}</p>
-          <p>{product.rating}</p>
-          <p>{product.photo}</p>
-        </div>
-      })
+      <Box sx={{display: 'flex'}}>
+        <CircularProgress sx={{margin: '0 auto', color: 'green'}}/>
+      </Box>
+      :
+      <div className="catalog__list">
+        {data.map((product) => {
+          return <Card className="product" key={product.id} sx={{maxWidth: 345}}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={product.photo}
+              alt={product.title}
+            />
+            <CardContent>
+              <Typography className="product__title" gutterBottom variant="h5" component="div">
+                {product.title}
+              </Typography>
+              <Typography className="product__description" variant="body2" color="text.secondary">
+                {product.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+
+
+          // <div className="catalog__item" key={product.id}>
+          //   <h2>{product.title}</h2>
+          //   <p>{product.price}</p>
+          //   <p>{product.description}</p>
+          //   <p>{product.rating}</p>
+          //   <p>{product.photo}</p>
+          // </div>
+        })
+        }
+      </div>
     }
   </main>
 }
